9be10df8d86c0e4f7d3c13e5abce6455d3987caf
finn <65142636+finnholland@users.noreply.github.com>
2023-08-28 22:11:25 +0930

    markdown

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className='h-screen w-screen content-center justify-center items-center flex'>
+      <body className={`${inter.className} h-screen w-screen justify-center items-center flex`}>
+        {children}
+      </body>
     </html>
   )
 }
