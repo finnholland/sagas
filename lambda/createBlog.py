31c3d86ddc67144781e1650a265836ac3531b2e8
finn <65142636+finnholland@users.noreply.github.com>
2023-09-08 15:21:52 +0930

    api get and requests

--- lambda/createBlog.py
+++ lambda/createBlog.py
@@ -3,18 +3,22 @@ import json
 import uuid
 import datetime
 
+client = boto3.resource('dynamodb').Table('sagas')
+type = 'blog'
+
 def lambda_handler(event, context):
     createdAt = datetime.datetime.now().isoformat()
+    dateId = datetime.datetime.now().replace(microsecond=0).isoformat()
     unique_id = str(uuid.uuid4())
     body = json.loads(event['body'])
-    client = boto3.resource('dynamodb').Table('sagas')
 
     response = client.put_item(
     TableName='sagas',
         Item={
-            'id': unique_id,
-            'title': body['title'],
+            'id': type+'-'+unique_id+'-'+dateId,
             'createdAt': createdAt,
+            'type': type,
+            'title': body['title'],
             'body': body['body'],
             'author': body['author'],
             'visible': body['visible'],
